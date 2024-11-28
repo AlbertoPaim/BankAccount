741fdbc112d98d35cf382b6528fe6e6b53262cd4
Alberto Paim <albertoopaim@gmail.com>
2024-11-28 11:39:16 -0300

    BankAccount class

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BankAccount {
+    private String costumerName;
+    private String billingAddress;
+    private int balance;
+    private List<String> transactions;
+
+    public BankAccount(String costumerName, String billingAddress, int inicialBalance) {
+        this.costumerName = costumerName;
+        this.billingAddress = billingAddress;
+        this.balance = inicialBalance;
+        this.transactions = new ArrayList<>();
+        this.transactions.add("Saldo inicial:" + inicialBalance);
+    }
+
+    public void deposito(int amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            this.transactions.add("Novo deposito de R$: " + amount);
+        }
+
+    }
+
+    public void saque(int amount) {
+        if (balance > amount) {
+            this.balance -= amount;
+            this.transactions.add("Novo saque de R$: " + amount);
+        }
+    }
+
+
+}
